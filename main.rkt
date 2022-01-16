cd743e048b98ebc249beca8e170d088bdf2bab49
Winston Weinert <winston@ml1.net>
2022-01-16 03:24:24 -0600

    use alternate naming convention for parameters

--- main.rkt
+++ main.rkt
@@ -8,15 +8,15 @@
            racket/cmdline
            racket/function
            racket/match)
-  (define dir (make-parameter (current-directory)))
-  (define verbosity (make-parameter 'error))
+  (define *dir* (make-parameter (current-directory)))
+  (define *verbosity* (make-parameter 'error))
   [command-line
    #:once-each
-   (["-d" "--directory"] user-specified-dir "Directory to save images to." (dir (string->path user-specified-dir)))
-   (["-v" "--verbosity"] user-specified-verbosity "Verbosity: fatal, error, warning, info, debug - default to error" (verbosity (string->symbol user-specified-verbosity)))
+   (["-d" "--directory"] user-specified-dir "Directory to save images to." (*dir* (string->path user-specified-dir)))
+   (["-v" "--verbosity"] user-specified-verbosity "Verbosity: fatal, error, warning, info, debug - default to error" (*verbosity* (string->symbol user-specified-verbosity)))
    #:args threads
    (with-logging-to-port (current-error-port)
      (thunk (for ([th threads])
               (match-define (list board thread-no) (string->board+thread-no th))
-              (4ch-download-thread-images board thread-no #:directory (dir))))
-     #:logger 4ch-logger (verbosity))])
+              (4ch-download-thread-images board thread-no #:directory (*dir*))))
+     #:logger 4ch-logger (*verbosity*))])
