545109687581812f807f8e50bb13541af9399786
Winston Weinert <winston@ml1.net>
2022-01-16 03:05:12 -0600

    Add info.rkt so it can be installed

---
+++ info.rkt
@@ -0,0 +1,9 @@
+#lang info
+
+(define version "0.1")
+
+(define deps '("base" "http-easy"))
+(define build-deps '())
+
+(define racket-launcher-names '("4chdl"))
+(define racket-launcher-libraries '("main.rkt"))
